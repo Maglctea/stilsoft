558d4dad7625967006f2e8c50e958b9cf1460461
Fortunella <sherbakovdaniil6@gmail.com>
2023-07-04 23:14:09 +0300

    * customized admin panel

--- transaction/admin.py
+++ transaction/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from transaction.models import Transaction
 
-admin.site.register(Transaction)
+
+@admin.register(Transaction)
+class HostedAppAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'sum', 'created_at')
+    list_filter = ('type',)
+    ordering = ('user', 'pk', 'created_at')
+    list_per_page = 30
+    search_fields = ('user', 'sum')
+    list_display_links = ('user',)
--- user/admin.py
+++ user/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from user.models import Balance, User
 
-admin.site.register(User)
-admin.site.register(Balance)
+
+@admin.register(User)
+class HostedAppAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_superuser', 'is_active', 'date_joined')
+    list_filter = ('is_superuser', 'is_active')
+    ordering = ('username', 'email', 'date_joined')
+    list_per_page = 30
+    search_fields = ('user', 'email')
+    list_display_links = ('username', 'email')
+
+
+@admin.register(Balance)
+class HostedAppAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
+    ordering = ('user', 'balance')
+    list_per_page = 30
+    search_fields = ('user', 'balance')
+    list_display_links = ('user',)
